eed0cdfd8a823db44cb29245398af8ef235058ba
Jeison Cordoba <jeisonsmit@gmail.com>
2025-08-25 17:03:15 -0500

    end of codigo

---
+++ LogicConcepts/SimplePayroll/SimplePayroll.cs
@@ -0,0 +1,39 @@
+﻿//using Shared;
+
+using System.Globalization;
+
+var continueV = "s";
+
+do
+{
+    Console.Write("Ingrese nombre: ");
+    var nombreInput = Console.ReadLine();
+
+    Console.Write("Ingrese número de horas trabajadas: ");
+    var horasInput = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Ingrese valor hora: ");
+    var valorHoraImput = double.Parse(Console.ReadLine()!);
+
+    Console.Write("Ingrese valor salario mínimo mensual: ");
+    var salarioMinimoImput = double.Parse(Console.ReadLine()!);
+
+    var salarioMensual = horasInput * valorHoraImput;    
+
+
+    if (salarioMensual > salarioMinimoImput)
+    {
+        Console.WriteLine($"Nombre : {nombreInput}");
+        Console.WriteLine($"Salario mensual: {salarioMensual.ToString("N")} ");
+        //Console.WriteLine($"Salario mensual: {salarioMensual.ToString("N2", new CultureInfo("es-ES "))} ");
+    }
+    else
+    {
+        Console.WriteLine($"Nombre : {nombreInput}");
+    }
+
+    Console.Write("Desea continuar [S/N]? : ");
+    continueV = Console.ReadLine()!.ToUpper();
+    Console.Write("-----------------------------------------------------------------------\n");
+
+} while (continueV.Equals("S", StringComparison.OrdinalIgnoreCase));
